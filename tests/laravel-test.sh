92899d608223762b4645db7c8bafe5a141916d69
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-06-29 23:49:08 +0300

    ci: package name

--- tests/laravel-test.sh
+++ tests/laravel-test.sh
@@ -11,7 +11,7 @@ cd ../laravel/
 
 echo "Adding package from source"
 sed -e 's|"type": "project",|&"repositories": [ { "type": "path", "url": "../psalm-plugin-laravel" } ],|' -i composer.json
-COMPOSER_MEMORY_LIMIT=-1 composer require --dev "psalm/plugin-laravel:*"
+COMPOSER_MEMORY_LIMIT=-1 composer require --dev "brokeyourbike/plugin-laravel:*"
 
 echo "Analyzing Laravel"
 ./vendor/bin/psalm -c ../psalm-plugin-laravel/tests/laravel-test-psalm.xml
--- tests/lumen-test.sh
+++ tests/lumen-test.sh
@@ -11,7 +11,7 @@ cd ../lumen/
 
 echo "Adding package from source"
 sed -e 's|"type": "project",|&"repositories": [ { "type": "path", "url": "../psalm-plugin-laravel" } ],|' -i composer.json
-COMPOSER_MEMORY_LIMIT=-1 composer require --dev "psalm/plugin-laravel:*"
+COMPOSER_MEMORY_LIMIT=-1 composer require --dev "brokeyourbike/plugin-laravel:*"
 
 echo "Analyzing Lumen"
 ./vendor/bin/psalm -c ../psalm-plugin-laravel/tests/lumen-test-psalm.xml
